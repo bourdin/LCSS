b4bf01066a4bf827ffb0b727c704a6b0e7b4af3c
Blaise Bourdin <bourdin@lsu.edu>
2019-06-20 15:02:50 -0500

    typo

--- LCSS_pbs.sh
+++ LCSS_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -A hpc_vfrac2017b
 #PBS -q workq
-#PBS -l nodes=2:ppn=16
+#PBS -l nodes=3:ppn=16
 #PBS -l walltime=06:00:00
 #PBS -o $PBS_JOBID.out
 #PBS -j oe
@@ -9,4 +9,4 @@
 #PBS -V
 
 cd $PBS_O_WORKDIR
-../python/LCSS_slurm.py --order ${ORDER} --scanningSpeed ${SCANNINGSPEEG} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --position 0  --postprocess --mpiexec mpirun --geofile ../python/LCSS2.geo --yamlfile ../python/LCSS.yaml --meshdir ../Meshes --forcemesh
+../python/LCSS_slurm.py --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --position 0  --postprocess --mpiexec mpirun --geofile ../python/LCSS2.geo --yamlfile ../python/LCSS.yaml --meshdir ../Meshes
