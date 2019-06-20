41e330c99bb73378e7ee0d7d184dbdc9f851dfe3
Blaise Bourdin <bourdin@lsu.edu>
2019-06-20 15:12:27 -0500

    Debugged PBS script

--- LCSS_pbs.sh
+++ LCSS_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -A hpc_vfrac2017b
 #PBS -q workq
-#PBS -l nodes=3:ppn=16
+#PBS -l nodes=10:ppn=16
 #PBS -l walltime=06:00:00
 #PBS -o $PBS_JOBID.out
 #PBS -j oe
@@ -9,5 +9,6 @@
 #PBS -V
 
 cd $PBS_O_WORKDIR
-echo ../python/LCSS_slurm.py --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --position 0  --postprocess --mpiexec mpirun --geofile ../python/LCSS2.geo --yamlfile ../python/LCSS.yaml --meshdir ../Meshes
-../python/LCSS_slurm.py --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --position 0  --postprocess --mpiexec mpirun --geofile ../python/LCSS2.geo --yamlfile ../python/LCSS.yaml --meshdir ../Meshes
+echo ../python/LCSS_slurm.py --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --position 0 --mpiexec mpirun --geofile ../python/LCSS2.geo --yamlfile ../python/LCSS.yaml --meshdir ../Meshes
+
+python ../python/LCSS_slurm.py --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --position 0 --mpiexec mpirun --geofile ../python/LCSS2.geo --yamlfile ../python/LCSS.yaml --meshdir ../Meshes
