c6b49fedf986a16a81bf0562746b8ae8a59d66e6
Blaise Bourdin <bourdin@mike1.(none)>
2019-06-25 11:53:23 -0500

    tweaked PBS scripts

--- LCSS_pbs.sh
+++ LCSS_pbs.sh
@@ -2,7 +2,7 @@
 #PBS -A hpc_vfrac2017b
 #PBS -q workq
 #PBS -l nodes=10:ppn=16
-#PBS -l walltime=01:00:00
+#PBS -l walltime=00:30:00
 #PBS -o $PBS_JOBID.out
 #PBS -j oe
 #PBS -N LCSS
@@ -10,7 +10,7 @@
 
 cd $PBS_O_WORKDIR
 echo "now running"
-echo ../python/LCSS_slurm.py --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --position 0 --mpiexec mpirun --geofile ../python/LCSS2.geo --yamlfile ../python/LCSS.yaml --meshdir ../Meshes
+echo   ../python/LCSS_slurm.py --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --r ${R} --position 0 --mpiexec mpirun --geofile ../python/LCSS2.geo --yamlfile ../python/LCSS2.yaml --meshdir ../Meshes
 echo "   "
-python ../python/LCSS_slurm.py --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --position 0 --mpiexec mpirun --geofile ../python/LCSS2.geo --yamlfile ../python/LCSS.yaml --meshdir ../Meshes
+python ../python/LCSS_slurm.py --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --r ${R} --position 0 --mpiexec mpirun --geofile ../python/LCSS2.geo --yamlfile ../python/LCSS2.yaml --meshdir ../Meshes
 
--- launcher_pbs.sh
+++ launcher_pbs.sh
@@ -4,16 +4,21 @@ import os
 
 width = 100
 height= 50
-scanningSpeed=5.0
-#hf=0.025 
-#hc=1.0
-#order=1
-hf=0.05 
-hc=2.0
+scanningSpeed=5.
+hf=0.1
+hc=0.5
+r=8
 order=2
-numl=101
 
-for l in np.linspace(30,50,numl):
-   cmd = "SCANNINGSPEED={0} HF={1} HC={2} WIDTH={3} HEIGHT={4} LC={5} ORDER={6} qsub ../python/LCSS_pbs.sh".format(scanningSpeed,hf,hc,width,height,l,order)
+nl = 201
+lmin = 50
+lmax = 70
+ll = np.linspace(lmin,lmax,nl)
+
+nc = 100
+step = 1
+
+for l in ll[nc*step:nc*(step+1)]:
+   cmd = "SCANNINGSPEED={0} HF={1} HC={2} WIDTH={3} HEIGHT={4} LC={5} ORDER={6} R={7} qsub ../python/LCSS_pbs.sh".format(scanningSpeed,hf,hc,width,height,l,order,r)
    print(cmd)
-   os.system(cmd)
+   #os.system(cmd)
