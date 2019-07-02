3fb693789280f85742d1ec7493fa72a18a8fea4d
Blaise Bourdin <bourdin@lsu.edu>
2019-07-02 10:57:42 -0500

    changes in PBS script

--- LCSS_pbs.sh
+++ LCSS_pbs.sh
@@ -10,7 +10,7 @@
 
 cd $PBS_O_WORKDIR
 echo "now running"
-echo   ../python/LCSS_slurm.py --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --r ${R} --position 0 --mpiexec mpirun --geofile ../python/LCSS2.geo --yamlfile ../python/LCSS2.yaml --meshdir ../Meshes
+echo   ../python/LCSS_slurm.py --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --r ${R} --position 0 --mpiexec mpirun --geofile ../python/LCSS3.geo --yamlfile ../python/LCSS3.yaml --meshdir ../Meshes
 echo "   "
-python ../python/LCSS_slurm.py --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --r ${R} --position 0 --mpiexec mpirun --geofile ../python/LCSS2.geo --yamlfile ../python/LCSS2.yaml --meshdir ../Meshes
+python ../python/LCSS_slurm.py --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --r ${R} --position 0 --mpiexec mpirun --geofile ../python/LCSS3.geo --yamlfile ../python/LCSS3.yaml --meshdir ../Meshes
 
