1134baefc8a968b608ca5db992fd80981e0abd21
Blaise Bourdin <bourdin@lsu.edu>
2019-08-05 10:52:45 -0500

    Added export script

---
+++ plotDamage.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for d in $@; do 
+    sbatch -n 1 --wrap "visit -cli -nowin -s ../python/LCSS_PNGDamage.py $d/${d}_out.gen --force --output $d/${d}_out-Ref.png      --displacementScaling 0 --damageThreshold 1. --BB -26 26 -26 26 --step 1";
+    sbatch -n 1 --wrap "visit -cli -nowin -s ../python/LCSS_PNGDamage.py $d/${d}_out.gen --force --output $d/${d}_out-Refzoom.png  --displacementScaling 0 --damageThreshold 1. --BB -20 5 -5 5    --step 1"; 
+done
