81fce51f5a442c5b92961d6f8ec59000f1ef70cf
Blaise Bourdin <bourdin@lsu.edu>
2019-09-30 22:57:55 -0500

    Unix is case-sensitive...

--- plotStress.sh
+++ plotStress.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for d in $@; do 
-    sbatch -n 1 --wrap "visit -cli -nowin -s $LCSS_DIR/python/LCSS_PNGSigmayy.py $d/${d}_out.gen --force --output $d/${d}_out-Sigmayy.png      --BB -26 26 -26 26 --step 1";
-    sbatch -n 1 --wrap "visit -cli -nowin -s $LCSS_DIR/python/LCSS_PNGSigmayy.py $d/${d}_out.gen --force --output $d/${d}_out-Sigmayyzoom.png  --BB -20 15 -5 5   --step 1"; 
+    sbatch -n 1 --wrap "visit -cli -nowin -s $LCSS_DIR/python/LCSS_PNGSigmaYY.py $d/${d}_out.gen --force --output $d/${d}_out-Sigmayy.png      --BB -26 26 -26 26 --step 1";
+    sbatch -n 1 --wrap "visit -cli -nowin -s $LCSS_DIR/python/LCSS_PNGSigmaYY.py $d/${d}_out.gen --force --output $d/${d}_out-Sigmayyzoom.png  --BB -20 15 -5 5   --step 1"; 
 done
