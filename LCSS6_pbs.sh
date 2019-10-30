6037070de64a6b667b13b0bb99e3a0575ea2c394
Blaise Bourdin <bourdin@lsu.edu>
2019-10-29 21:12:55 -0500

    I hate git

--- LCSS6_pbs.sh
+++ LCSS6_pbs.sh
@@ -1,1 +1,6 @@
+cd $PBS_O_WORKDIR
+echo "now running"
+echo   ${LCSS_DIR}/LCSS_slurm.py --internalLength ${ELL} --fractureToughness ${GC} --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --lf ${LF} --r ${R} --intensity ${INTENSITY} --initialPos ${X0} 0 0 --finalPos ${XF} 0 0 --time_numstep 1 --mpiexec mpirun --geofile ${LCSS_DIR}/LCSS6.geo --yamlfile ${LCSS_DIR}/LCSS5-AT1StaticHD.yaml --meshdir ${PBS_O_WORKDIR}/Meshes
+echo "   "
+python ${LCSS_DIR}/LCSS_slurm.py --internalLength ${ELL} --fractureToughness ${GC} --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --lf ${LF} --r ${R} --intensity ${INTENSITY} --initialPos ${X0} 0 0 --finalPos ${XF} 0 0 --time_numstep 1 --mpiexec mpirun --geofile ${LCSS_DIR}/LCSS6.geo --yamlfile ${LCSS_DIR}/LCSS5-AT1StaticHD.yaml --meshdir ${PBS_O_WORKDIR}/Meshes
 
