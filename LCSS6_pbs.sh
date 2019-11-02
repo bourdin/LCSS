35a64cad5894244c67617ac0bf1140a77f9689d0
Blaise Bourdin <bourdin@lsu.edu>
2019-11-02 17:43:16 -0500

    Fixed LCSS6 pbs script

--- LCSS6_pbs.sh
+++ LCSS6_pbs.sh
@@ -1,3 +1,10 @@
+#PBS -A hpc_vfrac2017b
+#PBS -q workq
+#PBS -l walltime=06:00:00
+#PBS -o $PBS_JOBID.out
+#PBS -j oe
+#PBS -N LCSS6
+#PBS -V
 cd $PBS_O_WORKDIR
 echo "now running"
 echo   ${LCSS_DIR}/LCSS_slurm.py --internalLength ${ELL} --fractureToughness ${GC} --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --lf ${LF} --r ${R} --intensity ${INTENSITY} --initialPos ${X0} 0 0 --finalPos ${XF} 0 0 --time_numstep ${NSTEP} --mpiexec mpirun --geofile ${LCSS_DIR}/LCSS6.geo --yamlfile ${LCSS_DIR}/LCSS5-AT1StaticHD.yaml --meshdir ${PBS_O_WORKDIR}/Meshes
