f900508e000a5dbd3b9a2b2cfeb8633c9dd80ec3
Blaise Bourdin <bourdin@lsu.edu>
2020-02-07 15:37:37 -0600

    Trying other KSP/PC combinations for the displacement solver

--- LCSS_pbs.sh
+++ LCSS_pbs.sh
@@ -1,16 +1,14 @@
-#!/bin/bash
 #PBS -A hpc_vfrac2017c
 #PBS -q workq
-#PBS -l nodes=10:ppn=16
-#PBS -l walltime=02:00:00
+#PBS -l walltime=06:00:00
 #PBS -o $PBS_JOBID.out
 #PBS -j oe
-#PBS -N LCSS
+#PBS -N LCSS6
 #PBS -V
-
 cd $PBS_O_WORKDIR
 echo "now running"
-echo   ${LCSS_DIR}/LCSS_slurm.py --internalLength ${ELL} --fractureToughness ${GC} --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --lf ${LF} --r ${R} --intensity ${INTENSITY} --position 0 --mpiexec mpirun --geofile ${LCSS_DIR}/LCSS5.geo --yamlfile ${LCSS_DIR}/LCSS5-AT1StaticHD.yaml --meshdir ${LCSS_DIR}/Meshes
+echo   ${LCSS_DIR}/LCSS_slurm.py --internalLength ${ELL} --fractureToughness ${GC} --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --lf ${LF} --r ${R} --intensity ${INTENSITY} --initialPos ${X0} 0 0 --finalPos ${XF} 0 0 --time_numstep ${NSTEP} --mpiexec mpirun --geofile ${LCSS_DIR}/LCSS6.geo --yamlfile ${LCSS_DIR}/{YAMLFILE} --meshdir ${PBS_O_WORKDIR}/Meshes
 echo "   "
-python ${LCSS_DIR}/LCSS_slurm.py --internalLength ${ELL} --fractureToughness ${GC} --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --lf ${LF} --r ${R} --intensity ${INTENSITY} --position 0 --mpiexec mpirun --geofile ${LCSS_DIR}/LCSS5.geo --yamlfile ${LCSS_DIR}/LCSS5-AT1StaticHD.yaml --meshdir ${LCSS_DIR}/Meshes
+python ${LCSS_DIR}/LCSS_slurm.py --internalLength ${ELL} --fractureToughness ${GC} --order ${ORDER} --scanningSpeed ${SCANNINGSPEED} --hf ${HF} --hc ${HC} --width ${WIDTH} --height ${HEIGHT} --lc ${LC} --lf ${LF} --r ${R} --intensity ${INTENSITY} --initialPos ${X0} 0 0 --finalPos ${XF} 0 0 --time_numstep ${NSTEP} --mpiexec mpirun --geofile ${LCSS_DIR}/LCSS6.geo --yamlfile ${LCSS_DIR}/{YAMLFILE} --meshdir ${PBS_O_WORKDIR}/Meshes
+
 
