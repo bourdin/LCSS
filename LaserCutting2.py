a9d3551144f9b9aca87072a40872ca43fda48263
Blaise Bourdin <bourdin@lsu.edu>
2020-08-03 16:13:07 -0500

    fixed a small bug in LaserCutting2.py

--- LaserCutting2.py
+++ LaserCutting2.py
@@ -16,9 +16,10 @@ import os
 os.chdir(os.getenv("PBS_O_WORKDIR"))
 jobid = os.getenv("PBS_JOBID")
 D = json.load(open("00_INFO.json"))
+lcssdir = os.getenv("LCSS_DIR")
 
-cmd1 = "~/ASAHI_2020_Summer/LaserCuttingScripts/LCSS_flux4.py -i {geometry:s} -o {result:s} --pathfile {pathfile:s} --cs 1 2 --d {d:.4e} --alpha {alpha:.4e} --r0 {r0:.4e}  --x0 {x0:.4e} --t0 {t0:.4e}  --dt {dt:.4e} --force | tee {0:s}.txt".format(jobid,**D)
-cmd2 = "mpirun vDef -geometry {geometry:s} -result {result:s} -options_file_yaml {yamlfile:s} | tee {0:s}.txt".format(jobid,**D)
+cmd1 = "{lcssdir}/LCSS_flux4.py -i {geometry:s} -o {result:s} --pathfile {pathfile:s} --cs 1 2 --d {d:.4e} --alpha {alpha:.4e} --r0 {r0:.4e}  --x0 {x0:.4e} --t0 {t0:.4e}  --dt {dt:.4e} --force | tee {jobid:s}.txt".format(jobid=jobid,lcssdir=lcssdir,**D)
+cmd2 = "mpirun vDef -geometry {geometry:s} -result {result:s} -options_file_yaml {yamlfile:s} | tee {jobid:s}.txt".format(jobid=jobid,**D)
 print(cmd1)
 os.system(cmd1)
 print(cmd2)
